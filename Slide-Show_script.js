ee486ad1df7c9ae8181353d7fc4bd59a75de1547
Vsevomes <jhonykattvil@gmail.com>
2024-04-24 12:41:28 +0300

    bag fix

--- Slide-Show_script.js
+++ Slide-Show_script.js
@@ -1,47 +1,120 @@
-// Массив url для перехода
-var urls = [];
+let starts = 0;
+
+new Vue({ 
+    el: '#app', 
+    data: { 
+        urls: [], 
+        showSlideshow: false, 
+        showAboutSection: false, 
+        showContactSection: false, 
+        showSuggestionsSection: false, 
+        slideshowInterval: null, 
+        currentImageIndex: 0, 
+        showAlert: false, 
+        isSlideshowStarted: false, 
+        allowTabSwitch: false, 
+        firstName: '', 
+        lastName: '', 
+        middleName: '', 
+        birthYear: '', 
+        movieSuggestion: '', 
+        comments: '',
+    }, 
+    created: function () { 
+        for (let i = 1; i != 194; i++) { 
+            this.urls.push("img/и" + i + ".jpg"); 
+        } 
+    }, 
+    methods: { 
+        submitForm: function() { 
+          alert('Форма отправлена!'); 
+        }, 
+        openUrl: function (url) { 
+            window.open(url, "_blank"); 
+        }, 
+        startSlideshow: function () { 
+     
+            this.isSlideshowStarted = true; 
+            this.showSlideshow = true; 
+            document.getElementById("button_id").hidden = true; 
+            if(starts == 0){
+                starts++;
+                new Audio("music/background_Music.mp3").play(); }
+            this.changeImage(); 
+        }, 
+        changeImage: function () { 
+            let images = document.getElementsByTagName("img"); 
+            for (var i = 0; i < images.length; i++) { 
+                images[i].style.display = "none"; 
+            } 
+            images[this.currentImageIndex].style.display = "block"; 
+            this.currentImageIndex = (this.currentImageIndex + 1) % images.length; 
+            this.slideshowInterval = setTimeout(this.changeImage, 300); 
+        },
+        toggleSlideshow: function () {
+            
+            if(!this.showSlideshow) return;
+            if (this.isSlideshowStarted) {
+                clearTimeout(this.slideshowInterval);
+                this.isSlideshowStarted = false;
+            } else {
+                this.startSlideshow();
+            }
+        },
+        stopSlideshow: function () {
+            clearTimeout(this.slideshowInterval);
+            this.isSlideshowStarted = false;
+        },
+        prevImage: function () {    
+            if(!this.showSlideshow) return;
+
+            this.stopSlideshow();
+            this.currentImageIndex = (this.currentImageIndex - 1 + this.urls.length) % this.urls.length;
+        },
+        nextImage: function () {
+            if(!this.showSlideshow) return;
+
+            this.stopSlideshow();
+            this.currentImageIndex = (this.currentImageIndex + 1) % this.urls.length;
+        },
+        showImages: function () { 
+            if (!this.isSlideshowStarted) { 
+                return; 
+            } 
+            this.showAboutSection = false; 
+            this.showContactSection = false; 
+            this.showSuggestionsSection = false; 
+            this.showSlideshow = true; 
+           // this.changeImage(); 
+        }, 
+        showAbout: function () { 
+     
+            this.showSlideshow = false; 
+            this.showContactSection = false; 
+            this.showSuggestionsSection = false; 
+            this.showAboutSection = true; 
+            clearTimeout(this.slideshowInterval); 
+        }, 
+        showSuggestions: function () { 
+
+
+          this.showSlideshow = false; 
+          this.showContactSection = false; 
+          this.showAboutSection = false; 
+          this.showSuggestionsSection = true; 
+          clearTimeout(this.slideshowInterval); 
+
+        }, 
+             
+        showContact: function () { 
+
+            this.showSlideshow = false; 
+            this.showAboutSection = false; 
+            this.showSuggestionsSection = false; 
+            this.showContactSection = true; 
+            clearTimeout(this.slideshowInterval); 
+
+        } 
+    } 
+  });
   
-for (let i = 1; i != 194; i++) {
-  urls.push("img/и" + i + ".jpg")
-}
-
-// Переход по ссылкам
-function openUrl(url) {
-  window.open(url, "_blank");
-}
-
-function start() {
-  document.getElementById("button_id").hidden=true 
-  new Audio("music/background_Music.mp3").play()
-  add_images()
-}
-
-function add_images() {
-  let div = document.getElementById("img_container")
-  for (let i = 1; i != 194; i++) {
-    let a = document.createElement('a')
-    a.href = "javascript:openUrl(urls[" + (i - 1) + "])"
-    let img = document.createElement('img')
-    img.src = "img/и" + i + ".jpg"
-    img.alt = i
-    a.appendChild(img)
-    div.appendChild(a)
-  }
-  var images = document.getElementsByTagName("img");
-  var currentImage = 0;
-
-  function changeImage() {
-    for (var i = 0; i < images.length; i++) {
-      images[i].style.display = "none";
-    }
-    images[currentImage].style.display = "block";
-    currentImage = (currentImage + 1) % images.length;
-  }
-
-  setInterval(changeImage, 300);
-}
-
-  document.addEventListener("DOMContentLoaded", function () {
-    let button = document.getElementById("button_id")
-    button.onclick = start
-  })
